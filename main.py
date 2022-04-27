ad161fb4dbac47fc39ca260fa6975af46b0fd496
Laddge <dev.laddge@gmail.com>
2022-04-27 06:04:26 +0000

    [feat] add api

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import os
 import time
 import secrets
 from urllib.parse import urlparse
-from fastapi import FastAPI, Request, Cookie
-from fastapi.responses import RedirectResponse, Response, HTMLResponse
+from fastapi import FastAPI, Request, Cookie, HTTPException
+from fastapi.responses import RedirectResponse, Response, HTMLResponse, StreamingResponse
 from typing import Optional
 import requests
 import cryptocode
@@ -61,6 +61,11 @@ def decrypt_token(encrypted_token):
         return
 
 
+async def get_content(r):
+    for chunk in r.iter_content(chunk_size=128):
+        yield chunk
+
+
 @app.get("/", response_class=HTMLResponse)
 async def get_root(encrypted_token: Optional[str] = Cookie(None)):
     if encrypted_token:
@@ -95,3 +100,58 @@ async def get_logout():
     response = RedirectResponse("/")
     response.set_cookie(key="encrypted_token", value="")
     return response
+
+
+@app.get("/api")
+async def get_api(
+        r: Optional[str] = None,
+        b: Optional[str] = None,
+        d: Optional[str] = None,
+        f: Optional[str] = None,
+        encrypted_token: Optional[str] = Cookie(None)):
+    if not r:
+        raise HTTPException(status_code=400)
+    token = decrypt_token(encrypted_token) if encrypted_token else None
+    if not b:
+        r0 = requests.get(
+            f"https://api.github.com/repos/{r}",
+            headers={"Authorization": f"token {token}"}
+        )
+        if r0.status_code != 200:
+            raise HTTPException(status_code=r0.status_code)
+        b = r0.json()["default_branch"]
+    if d:
+        if f:
+            if f[-3:] not in ["mp3", "m4a", "wav", "ogg"]:
+                raise HTTPException(status_code=400)
+            r1 = requests.get(
+                f"https://raw.githubusercontent.com/{r}/{b}{d}/{f}",
+                headers={"Authorization": f"token: {token}"},
+                stream=True
+            )
+            if r1.status_code != 200:
+                raise HTTPException(status_code=r1.status_code)
+            return StreamingResponse(get_content(r1))
+        r1 = requests.get(
+            f"https://api.github.com/repos/{r}/contents{d}?ref={b}",
+            headers={"Authorization": f"token {token}"}
+        )
+        if r1.status_code != 200:
+            raise HTTPException(status_code=r1.status_code)
+        files = []
+        for obj in r1.json():
+            if obj["type"] == "file":
+                if obj["name"][-3:] in ["mp3", "m4a", "wav", "ogg"]:
+                    files.append(obj["name"])
+        return files
+    r1 = requests.get(
+        f"https://api.github.com/repos/{r}/git/trees/{b}?recursive=true",
+        headers={"Authorization": f"token {token}"}
+    )
+    if r1.status_code != 200:
+        raise HTTPException(status_code=r1.status_code)
+    trees = ["/"]
+    for tree in r1.json()["tree"]:
+        if tree["type"] == "tree":
+            trees.append("/" + tree["path"])
+    return trees
