b226a4f94651c1118da5141bf015422569a40ccb
Laddge <dev.laddge@gmail.com>
2022-05-03 14:10:17 +0000

    [fix] fix way to get apic

--- main.py
+++ main.py
@@ -88,7 +88,7 @@ def get_audio(name, url, headers):
         tags = ID3(BytesIO(r2.content))
         audio["title"] = tags.get("TIT2").text[0] if tags.get("TIT2") else ""
         audio["artist"] = tags.get("TPE1").text[0] if tags.get("TPE1") else ""
-        apic = tags.get("APIC:")
+        apic = tags.getall("APIC")[0]
         audio["apic"] = base64.b64encode(apic.data).decode() if apic else ""
         audio_file = mutagen.File(BytesIO(r2.content))
         audio["length"] = audio_file.info.length
